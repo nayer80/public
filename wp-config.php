0cb75d5ff3458c51c84e190e1d7758f9d3a9698f
shicomanzour-tech <shico.manzour@gmail.com>
2025-12-05 18:31:48 +0200

    security: disable WP_DEBUG and WP_DEBUG_LOG in production config

--- wp-config.php
+++ wp-config.php
@@ -88,11 +88,11 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', true );
+	define( 'WP_DEBUG', false );
 }
 
 if ( ! defined( 'WP_DEBUG_LOG' ) ) {
-	define( 'WP_DEBUG_LOG', true );
+	define( 'WP_DEBUG_LOG', false );
 }
 
 if ( ! defined( 'WP_DEBUG_DISPLAY' ) ) {
